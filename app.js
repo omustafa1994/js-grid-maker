f93a70f2d2f1036c0a6023826329dea74c30cdd9
omustafa1994 <omustafa1994@gmail.com>
2018-11-29 13:26:59 +0000

    push this

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+
 document.addEventListener('DOMContentLoaded', function () {
   // Add code here
   const board = document.getElementById('board');
 
 
+  grid.createGridSize = (rowSize, colSize) => {
+    for (let i = 1; i <= rowSize; i++) {
+      const row = document.createElement("div");
+      div.setAtribute("class", "row");
+
+      for (let j = 1; j <= 5; j++) {
+        const div = document.createElement("div");
+        div.setAtribute("class", "col");
+      }
+
+      row.appendChild(col);
+
+    }
+
+  });
 
 
 
-});
